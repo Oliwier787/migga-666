ea33e1d7caa2dd7beef54d8a758e3dd188a32e41
Oliwier Zawierzeniec <oliwier.zawierzeniec@teb.edu.pl>
2024-02-28 15:14:35 +0100

    dd

--- Assets/CCTVCameraController.cs
+++ Assets/CCTVCameraController.cs
@@ -13,10 +13,14 @@ public class CCTVCameraController : MonoBehaviour
     //czy kamera kr?ci si? w prawo
     bool turningRight = !true;
 
+    //obiektyw kamery
+    Transform cameraLens;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        Transform cameraPosition = transform.Find("CameraPosition");
+        cameraLens = cameraPosition.Find("Cylinder");
     }
 
     // Update is called once per frame
@@ -26,6 +30,13 @@ public class CCTVCameraController : MonoBehaviour
         // oscyluj?ce pomi?dzy 0 a 9, a nast?pnie mno?ymy to przez 10 ?eby uzyska? szybszy ruch
         // i na koniec odejmujemy od warto?ci otrzymanej 45 stopni aby uzyska? ruch w zakresie -45 do 45
         transform.rotation = Quaternion.Euler(new Vector3(0, Mathf.PingPong(Time.time, 9) * 10 - 45, 0));
+
+        CheckIfPlayerVisible();
+    }
+
+    void CheckIfPlayerVisible()
+    {
+        Debug.DrawRay(cameraLens.position, cameraLens.TransformDirection(Vector3.down) * 100, Color.yellow);
     }
 
 }
